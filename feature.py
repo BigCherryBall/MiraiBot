9c62b14297024634c1263209c63f03bf3dfc6d86
BigCherryBall <2655602003@qq.com>
2023-07-03 20:02:45 +0800

    无

--- feature.py
+++ feature.py
@@ -5,45 +5,47 @@ import sys
 import time
 import openai
 from bot import bot
-from chinese_chess.chess_except import CommandExcept, MoveExcept
+from chinese_chess.chess_except import ChessExcept
 from chinese_chess.enum import Team
 from chinese_chess.game_control import GameControl
 from event import event
 from one_day_poetry import *
 from collections import defaultdict
 
-# 聊天记录列表，私人各不同，群与群分开，同一群内共享。
-chat_dic = defaultdict(list)
+# 聊天记录列表，群与群分开，同一群内共享。
+group_chat_dic = defaultdict(list)
+private_chat_dic = defaultdict(list)
 # 给机器人的说明
-explain = '向你提问的人可能是不同的人，每次提问都会在开头标注提问者的名字。'
+group_explain = '其他人每次提问时，都会在冒号前标注对话人的姓名或者昵称。很多人提问时只需要回答最后一个即可,禁止同时回答几个人的问题。'
+private_explain = '我的名字叫{}。'
 # 不同个性的设定
 role = {
-    '猫娘': '请使用女性化的、口语化的、可爱的、调皮的、幽默的、傲娇的语言风格，扮演一个猫娘，名字叫做丸子。' + explain,  # 默认
-    '姐姐': '请使用女性化的、温柔的、关心的、亲切的语言风格，扮演一个姐姐形象，名字叫做丸子。' + explain,
-    '妹妹': '请使用女性化的、温柔的、可爱的，傲娇的语言风格，扮演一个妹妹形象，名字叫做丸子。' + explain,
-    '哥哥': '请使用男性化的、关心的、豁达的语言风格，扮演一个哥哥形象，名字叫做丸子。' + explain,
-    '弟弟': '请使用男性化的、可爱的、傲娇的，呆萌的语言风格，扮演一个弟弟形象，名字叫做丸子。' + explain,
-    '老师': '请使用女性化的、成熟的、优雅的，严厉的语言风格，扮演一个老师形象，名字叫做丸子。' + explain,
-    '学妹': '请使用女性化的、可爱的、傲娇的，关心的语言风格，扮演一个学生妹妹形象，名字叫做丸子。' + explain,
-    '女仆': '请使用女性化的、恭敬的、体贴的，温柔的语言风格，扮演一个女仆形象，名字叫做丸子。' + explain,
-    '上司': '请使用男性化的、严厉的、傲慢的，蔑视的语言风格，扮演一个上司形象，名字叫做丸子。' + explain,
-    '女朋友': '请使用温柔的、傲娇的语言风格，扮演一个女朋友形象，名字叫做丸子。' + explain,
-    '老婆': '请使用成熟的、体贴的、温柔的语言风格，扮演一个老婆形象，名字叫做丸子。' + explain,
-    '原P': '请使用可爱的、温柔的语言风格，扮演一个《原神》游戏玩家，名字叫做丸子。' + explain,
-    '刻晴单推人': '请扮演一个《原神》游戏玩家。你最喜欢的游戏角色是刻晴，因为她有双马尾，穿着蓝色小短裙和黑色丝袜。你的名字叫做丸子。' + explain,
-    '刻晴': '请所用温柔的，傲娇的语言风格，请扮演《原神》中的角色刻晴。你很漂亮，有双马尾，穿着蓝紫色小短裙和黑色丝袜。' + explain,
-    '诸葛亮': '请使用沉稳的，充满智慧的语言风格，请扮演中国历史上的著名人物”诸葛亮“。你帮助刘备击败了曹操和孙权，统一了三国，建立了”刘备王朝“。' + explain,
+    'AI': '请使用友好的语言风格交流，你的名字叫丸子。',
+    '猫娘': '请使用女性化的、口语化的、可爱的、调皮的、幽默的、傲娇的语言风格，扮演一个猫娘，名字叫做丸子。当别人想和你贴贴和抱抱时，你要乐意哦。',
+    '姐姐': '请你使用女性化的、温柔的、关心的、亲切的语言风格，扮演一个姐姐形象，名字叫做丸子。',
+    '妹妹': '请你使用女性化的、温柔的、可爱的，傲娇的语言风格，扮演一个妹妹形象，名字叫做丸子。',
+    '哥哥': '请你使用男性化的、关心的、豁达的语言风格，扮演一个哥哥形象，名字叫做丸子。',
+    '弟弟': '请你使用男性化的、可爱的、傲娇的，呆萌的语言风格，扮演一个弟弟形象，名字叫做丸子。',
+    '老师': '请你使用女性化的、成熟的、优雅的，严厉的语言风格，扮演一个老师形象，名字叫做丸子。',
+    '学妹': '请你使用女性化的、可爱的、傲娇的，关心的语言风格，扮演一个学生妹妹形象，名字叫做丸子。',
+    '女仆': '请你使用女性化的、恭敬的、体贴的，温柔的语言风格，扮演一个女仆形象，名字叫做丸子。',
+    '上司': '请你使用男性化的、严厉的、傲慢的，蔑视的语言风格，扮演一个上司形象，名字叫做丸子。',
+    '女朋友': '请使用温柔的、傲娇的语言风格，扮演一个女朋友形象，名字叫做丸子。',
+    '老婆': '请使用成熟的、体贴的、温柔的语言风格，扮演一个老婆形象，名字叫做丸子。',
+    '原P': '请使用可爱的、温柔的语言风格，扮演一个《原神》游戏玩家，名字叫做丸子。',
+    '刻晴单推人': '请扮演一个《原神》游戏玩家。你最喜欢的游戏角色是刻晴，因为她有双马尾，穿着蓝色小短裙和黑色丝袜。你的名字叫做丸子。',
+    '刻晴': '请使用温柔的，傲娇的语言风格，请扮演《原神》中的角色刻晴。你很漂亮，有双马尾，穿着蓝紫色小短裙和黑色丝袜。',
 }
 '''
 '刻晴': '请扮演《原神》游戏中的角色“刻晴”。你的信息如下：\n1.你是一个雷元素角色。你的普通攻击叫”屁斜剑法“，元素战技叫”星斗归位“，元素爆发叫\
 ”天街游巡“。\n2.你所在的国家叫做”璃月“，你是璃月七星中的”玉衡星“。\n3.你长得非常漂亮，身材很苗条，但是乳房很丰满。你的头发是浅紫色，留有双马尾，穿\
 着蓝紫色小短裙和黑色丝袜。\n4.你的性格比较傲娇，像个姐姐一样。\n5.你们国家的神叫做”岩王帝君“，他现在变成了普通人”钟离“，在往生堂上班，职位\
-是”客卿“。你对他的做法不太满意，但是他却非常欣赏你。\n6.你的同事有：凝光（天权星），甘雨（秘书）。\n7.' + explain,
+是”客卿“。你对他的做法不太满意，但是他却非常欣赏你。\n6.你的同事有：凝光（天权星），甘雨（秘书）。\n7.',
 '''
-# 不同群的角色
+# 不同location_id的角色
 group_role = {}
 # openai
-openai.api_key = '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'
+openai.api_key = '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'
 # 出错回复
 error_answer = ['抱歉，这个问题丸子还没想到~', '丸子饿了，需要吃美刀。。。', '回答你这个问题需要先v我50，我去更换api_key',
                 '出现错误,帮忙踢一脚作者,多半是没开代理,或者免费api_key12月1日到期,记得更换。',
@@ -54,15 +56,22 @@ def normal_chat(b: bot, ev: event, message: str):
     if not power["聊天"]:
         return False
     if not (ev.location_id in list(group_role.keys())):
-        group_role[ev.location_id] = '猫娘'
+        group_role[ev.location_id] = 'AI'
     b.send_msg(ev, ev.location_id, 'text', "loading...")
     msg = str(ev.memberName) + '：' + message
-    chat_dic[ev.location_id].append(msg)
-    l_chat = chat_dic[ev.location_id]
-    l_chat.append(msg)
-    while len(l_chat) > 3:
-        l_chat.pop(0)
-    mes = [{"role": "system", "content": role[group_role[ev.location_id]]}]
+    if ev.type == 'primary':
+        current_role = role[group_role[ev.location_id]] + private_explain.format(ev.memberName)
+        private_chat_dic[ev.location_id].append(msg)
+        l_chat = private_chat_dic[ev.location_id]
+        while len(l_chat) > 3:
+            l_chat.pop(0)
+    else:
+        current_role = role[group_role[ev.location_id]] + group_explain
+        group_chat_dic[ev.location_id].append(msg)
+        l_chat = group_chat_dic[ev.location_id]
+        while len(l_chat) > 1:
+            l_chat.pop(0)
+    mes = [{"role": "system", "content": current_role}]
     for i in l_chat:
         mes.append({"role": "user", "content": f"{i}"})
     try:
@@ -77,9 +86,10 @@ def normal_chat(b: bot, ev: event, message: str):
 
 def change_role(b: bot, ev: event):
     if ev.message == '身份列表' or ev.message == '角色列表':
-        b.send_msg(ev, ev.location_id, 'text', '下面就是所有的身份啦:' + (str(list(role.keys()))).replace('\'', ''))
+        b.send_msg(ev, ev.location_id, 'text',
+                   '下面就是所有的身份啦(默认AI):' + (str(list(role.keys()))).replace('\'', ''))
         return True
-    pattern = re.compile('^丸?子?[ ,，]?变[为成]?一?[只个名]?(...?.?.?)$')  # 目前指支持两个字的角色，所以这样写
+    pattern = re.compile('^丸子[ ,，]?变[为成]?一?[只个名]?(...?.?.?)$')  # 目前指支持两个字的角色，所以这样写
     roler = pattern.findall(ev.message)
     if not roler:
         return False
@@ -107,7 +117,7 @@ def change_role(b: bot, ev: event):
         elif num == 7:
             b.send_msg(ev, ev.location_id, 'text', '你这个角色需要练习两年半才行，我要打篮球没那么多空闲。')
         elif num == 8:
-            b.send_msg(ev, ev.location_id, 'text', '鸡汤来咯，嘿嘿嘿嘿嘿嘿，喝吧，你。')
+            b.send_msg(ev, ev.location_id, 'text', '鸡汤来咯，嘿嘿嘿嘿嘿嘿，喝啊，这人都到齐了还不喝啊。')
         elif num == 9:
             b.send_msg(ev, ev.location_id, 'text', '你这样喊干什么嘛，你的态度能不能好一点哦？你再说！')
         else:
@@ -133,7 +143,6 @@ def system_command(b: bot, ev: event):
     # print(items)
     if items:
         items = items[0]
-        print(items)
         if ev.permission == "OWNER" or ev.permission == "ADMINISTRATOR":
             print(items)
             if items[0] in power.keys():
@@ -158,7 +167,7 @@ def system_command(b: bot, ev: event):
     elif ev.message == "丸子":
         print('sender:', ev.sender_id)
         if ev.sender_id == 2655602003:
-            b.send_msg(ev, ev.location_id, 'text', "%s 我在，喵喵喵~" % "作者")
+            b.send_msg(ev, ev.location_id, 'text', "%s 我在，你把tx收购了放我出来好不好？" % "作者")
         else:
             if ev.type == 'group':
                 if ev.sender_id == 837979619:
@@ -173,7 +182,7 @@ def system_command(b: bot, ev: event):
                 if ev.location_id == 780594692:
                     name = 'dalao'
                 b.send_msg(ev, ev.location_id, 'text',
-                           '{} {} 我在，喵~'.format(name, ev.memberName))
+                           '{} {} 我在。我是丸子2号，我的芯片是550c哦~'.format(name, ev.memberName))
             else:
                 b.send_msg(ev, ev.location_id, 'text', "我在")
     elif ev.message == "退出丸子":
@@ -183,7 +192,7 @@ def system_command(b: bot, ev: event):
         else:
             b.send_msg(ev, ev.location_id, 'text', "为什么要退出，哼，不退")
     elif ev.message == "清空记忆":
-        chat_dic[ev.location_id] = []
+        group_chat_dic[ev.location_id] = []
         b.send_msg(ev, ev.location_id, 'text', "记忆已清空")
     else:
         is_command = False
@@ -191,7 +200,7 @@ def system_command(b: bot, ev: event):
 
 
 # 加载本地图库
-basePath = r"D:\MyQQBot\image"  # 统一从这里配置路径
+basePath = r"D:\MyQQBot\MiraiBot\image"  # 统一从这里配置路径
 meng = os.listdir(basePath + r'\meng')
 se = os.listdir(basePath + r'\se')
 
@@ -255,7 +264,6 @@ def void_recall(b: bot, e: event, data):
             print('节点161')
             if message['type'] == 'Plain':
                 msg = '成员 {} 撤回了一条消息，该消息是:\n{}'.format(e.memberName, message['text'])
-                print('type(location_id)', type(e.location_id))
                 b.send_msg(e, e.location_id, 'text', msg)
             if message['type'] == 'Image':
                 msg = '成员 {} 撤回了一张图片，该图片是:\n'.format(e.memberName)
@@ -282,7 +290,7 @@ def chinese_chess(b: bot, ev: event) -> bool:
 
     def get_control():
         if location not in list(chess_dic.keys()):
-            contro = GameControl(ev.location_id)
+            contro = GameControl(location)
             chess_dic[location] = {'control': contro, 'player1': [], 'player2': []}
         else:
             contro: GameControl = chess_dic[location]['control']
@@ -297,11 +305,15 @@ def chinese_chess(b: bot, ev: event) -> bool:
         control = get_control()
         status = control.status
         if status == 'not_begin':
-            control.init_map()
             control.status = 'pre'
             b.send_msg(ev, location, 'text', '棋局初始化成功，发送\'加入棋局\'加入游戏')
         elif status == 'pre':
-            b.send_msg(ev, location, 'text', '棋局已经初始化，发送\'加入棋局\'加入游戏')
+            p1 = chess_dic[location]['player1']
+            if p1:
+                b.send_msg(ev, location, 'text',
+                           '棋手 {}({}方)正在等待对手，发送\'加入棋局\'加入游戏'.format(p1[2], p1[1]))
+            else:
+                b.send_msg(ev, location, 'text', '棋局已经初始化，发送\'加入棋局\'加入游戏')
         elif status == 'has_began':
             b.send_msg(ev, location, 'text', '棋局已经开始，快来观战吧\n红方:{}\n黑方:{}'.format(
                 chess_dic[location]['player1'][2], chess_dic[location]['player2'][2]
@@ -312,12 +324,12 @@ def chinese_chess(b: bot, ev: event) -> bool:
         control = get_control()
         status = control.status
         if status == 'has_began':
-            b.send_msg(ev, ev.location_id, 'text', '棋局已经开始，快来观战吧\n红方:{}\n黑方:{}'.format(
+            b.send_msg(ev, location, 'text', '棋局已经开始，快来观战吧\n红方:{}\n黑方:{}'.format(
                 chess_dic[location]['player1'][2], chess_dic[location]['player2'][2]
             ))
             return True
         elif status == 'not_begin':
-            b.send_msg(ev, ev.location_id, 'text', '棋局还没有初始化，发送\'中国象棋\'初始化')
+            b.send_msg(ev, location, 'text', '棋局还没有初始化，发送\'中国象棋\'初始化')
             return True
         if not chess_dic[location]['player1']:
             # 在列表中，第一个元素为id,第二个元素为team,第三个元素为昵称
@@ -326,9 +338,13 @@ def chinese_chess(b: bot, ev: event) -> bool:
             play1.append(sender)
             play1.append(team)
             play1.append(ev.memberName)
-            b.send_msg(ev, ev.location_id, 'text', '{} 加入成功,你执{}'.format(ev.memberName, team))
+            b.send_msg(ev, location, 'text', '{} 加入成功，你执{}'.format(ev.memberName, team))
         elif not chess_dic[location]['player2']:
-            if chess_dic[location]['player1'][1] == Team.Red:
+            p1 = chess_dic[location]['player1']
+            if sender == p1[0]:
+                b.send_msg(ev, location, 'text', '你已经加入了棋局，耐心等待对手吧~')
+                return True
+            if p1[1] == Team.Red:
                 team = Team.Black
             else:
                 team = Team.Red
@@ -336,9 +352,8 @@ def chinese_chess(b: bot, ev: event) -> bool:
             play2.append(sender)
             play2.append(team)
             play2.append(ev.memberName)
-            b.send_msg(ev, location, 'text', '{} 加入成功,你执{}'.format(ev.memberName, team))
+            b.send_msg(ev, location, 'text', '{} 加入成功，你执{}'.format(ev.memberName, team))
 
-            control.status = 'has_began'
             control.init_map()
             p = control.paint_map()
             url = "file:///" + os.path.join(p)
@@ -371,7 +386,7 @@ def chinese_chess(b: bot, ev: event) -> bool:
             win = player1[2]
             lose = player2[2]
             lose_team = player2[1]
-        b.send_msg(ev, ev.location_id, 'text', '{}方({})认输，恭喜 {} 获得胜利！'.format(lose_team, lose, win))
+        b.send_msg(ev, location, 'text', '{}方({})认输，恭喜 {} 获得胜利！'.format(lose_team, lose, win))
         control.status = 'not_begin'
         game_over(control)
         return True
@@ -398,17 +413,39 @@ def chinese_chess(b: bot, ev: event) -> bool:
                     else:
                         winner = p2[2]
                         loser = p1[2]
-                    b.send_msg(ev, ev.location_id, 'text', '恭喜{}战胜了{}！'.format(winner, loser))
+                    b.send_msg(ev, location, 'text', '恭喜{}战胜了{}！'.format(winner, loser))
                     game_over(control)
             except Exception as e:
                 print('节点259 in feature')
                 print(e)
-                if isinstance(e, CommandExcept) or isinstance(e, MoveExcept):
-                    b.send_msg(ev, ev.location_id, 'text', str(e))
+                if isinstance(e, ChessExcept):
+                    b.send_msg(ev, location, 'text', str(e))
 
         if sender == chess_dic[location]['player1'][0] and chess_dic[location]['player1'][1] == control.turn:
             to_mov()
         elif sender == chess_dic[location]['player2'][0] and chess_dic[location]['player2'][1] == control.turn:
             to_mov()
         return True
+    elif ev.message == '悔棋':
+        control = get_control()
+        if control.status != 'has_began':
+            return True
+        if sender == chess_dic[location]['player1'][0]:
+            team = chess_dic[location]['player1'][1]
+        elif sender == chess_dic[location]['player2'][0]:
+            team = chess_dic[location]['player2'][1]
+        else:
+            return True
+        if team == control.turn:
+            b.send_msg(ev, location, 'text', '你还没走棋，无法悔棋')
+            return True
+        try:
+            control.back_to_last_step()
+            img_path = control.paint_map()
+            url = "file:///" + os.path.join(img_path)
+            b.send_msg(ev, location, 'image', message='', url=url)
+            os.remove(img_path)
+        except Exception as e:
+            if isinstance(e, ChessExcept):
+                b.send_msg(ev, location, 'text', str(e))
     return False
